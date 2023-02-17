59c3e48148b96e30fc3d7a2ecbcafb6f90ba48ef
[tekilit] <67770867+tekilit@users.noreply.github.com>
2023-02-17 22:26:13 +0100

    feat : add boards gamer and machine

--- src/script.py
+++ src/script.py
@@ -1,14 +1,12 @@
 import random
 import numpy as np
 
-tablero = np.full((10, 10), " ")
 
 def barcos_a_tablero(num_barcos, eslora):
 
 
     while num_barcos > 0:
-        
-        print(num_barcos)
+
     # 'N' - 'S' - 'E' - 'O'
         orient = random.choice(['N', 'S', 'E', 'O'])
 
@@ -32,32 +30,23 @@ def barcos_a_tablero(num_barcos, eslora):
             tablero[fila:fila - eslora:-1, col] = 'O'
             num_barcos = num_barcos -1
 
-            
-           
 
     # Orientacion Este
         elif orient == 'E' and 0 <= col + eslora < 10  and 'O' not in coors_posiE:
             tablero[fila, col: col + eslora] = 'O'
             num_barcos = num_barcos -1
-            print("numero de barcos: ", num_barcos)
-            
-            
+
 
     # Orientacion Sur
         elif orient == 'S' and 0 <= fila + eslora < 10  and 'O' not in coors_posiS:
             tablero[fila:fila + eslora, col] = 'O'
             num_barcos = num_barcos -1
-            print("numero de barcos: ", num_barcos)
-            
-            
+
 
         # Orientacion Oeste
         elif orient == 'O' and 0 <= col - eslora < 10 and   'O' not in coors_posiO:
             tablero[fila, col: col - eslora:-1] = 'O'
             num_barcos = num_barcos -1
-            print("numero de barcos: ", num_barcos)
-            
-            
 
     # No cumple con las dimensiones del tablero, o hay un barco ahi
     # Probamos con otra coordenada
@@ -65,7 +54,42 @@ def barcos_a_tablero(num_barcos, eslora):
             continue
     return tablero
     
-barcos_a_tablero(2,3)
-barcos_a_tablero(4,1)
-barcos_a_tablero(3,2)
-barcos_a_tablero(1,4)
+tablero =np.full((10, 10), " ")
+def tablero_barcos_jugador():
+
+    barcos_3e = barcos_a_tablero(2,3)
+    barcos_4e = barcos_a_tablero(4,1)
+    barcos_2e = barcos_a_tablero(3,2)
+    barcos_1e = barcos_a_tablero(1,4)
+    
+    return tablero
+
+    
+jugador_barcos = tablero_barcos_jugador()
+
+print("""                  """)
+print("""
+┌┬┐┌─┐┌┐ ┬  ┌─┐┬─┐┌─┐  ┌┐ ┌─┐┬─┐┌─┐┌─┐┌─┐   ┬┬ ┬┌─┐┌─┐┌┬┐┌─┐┬─┐
+ │ ├─┤├┴┐│  ├┤ ├┬┘│ │  ├┴┐├─┤├┬┘│  │ │└─┐   ││ ││ ┬├─┤ │││ │├┬┘
+ ┴ ┴ ┴└─┘┴─┘└─┘┴└─└─┘  └─┘┴ ┴┴└─└─┘└─┘└─┘  └┘└─┘└─┘┴ ┴─┴┘└─┘┴└─ """ )
+print("""                  """)
+print(jugador_barcos)
+
+tablero =np.full((10, 10), " ")
+def tablero_barcos_maquina():
+
+        barcos_3e = barcos_a_tablero(2,3)
+        barcos_4e = barcos_a_tablero(4,1)
+        barcos_2e = barcos_a_tablero(3,2)
+        barcos_1e = barcos_a_tablero(1,4)
+
+        return tablero
+
+maquina_barcos = tablero_barcos_maquina()
+print("""                  """)
+print("""
+┌┬┐┌─┐┌┐ ┬  ┌─┐┬─┐┌─┐  ┌┐ ┌─┐┬─┐┌─┐┌─┐┌─┐  ┌┬┐┌─┐┌─┐ ┬ ┬┬┌┐┌┌─┐
+ │ ├─┤├┴┐│  ├┤ ├┬┘│ │  ├┴┐├─┤├┬┘│  │ │└─┐  │││├─┤│─┼┐│ │││││├─┤
+ ┴ ┴ ┴└─┘┴─┘└─┘┴└─└─┘  └─┘┴ ┴┴└─└─┘└─┘└─┘  ┴ ┴┴ ┴└─┘└└─┘┴┘└┘┴ ┴ """)
+print("""                  """)
+print(maquina_barcos)
