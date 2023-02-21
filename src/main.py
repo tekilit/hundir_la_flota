5375ec2b9d2f124e7e5d9df3cb59cbd6ad6d7b64
[tekilit] <67770867+tekilit@users.noreply.github.com>
2023-02-21 12:30:03 +0100

    feat: refactor and change prints

--- src/main.py
+++ src/main.py
@@ -2,19 +2,25 @@
 import functions as fn
 import numpy as np
 
-
-  
-jugador_barcos = fn.tablero_barcos_jugador()
-
-
-maquina_barcos = fn.tablero_barcos_maquina()
-
-
 if fn.ataca_jugador() == False:
-    print("Enhorabuena jugador has ganado")
-    tablero =np.full((10, 10), " ")
-    maquina_barcos = tablero
+    print("""
+╔═╗╔╗╔╦ ╦╔═╗╦═╗╔═╗╔╗ ╦ ╦╔═╗╔╗╔╔═╗                  
+║╣ ║║║╠═╣║ ║╠╦╝╠═╣╠╩╗║ ║║╣ ║║║╠═╣                  
+╚═╝╝╚╝╩ ╩╚═╝╩╚═╩ ╩╚═╝╚═╝╚═╝╝╚╝╩ ╩                  
+┬ ┬┌─┐┌─┐  ┌─┐┌─┐┌┐┌┌─┐┌┬┐┌─┐   ╦╦ ╦╔═╗╔═╗╔╦╗╔═╗╦═╗
+├─┤├─┤└─┐  │ ┬├─┤│││├─┤ │││ │   ║║ ║║ ╦╠═╣ ║║║ ║╠╦╝
+┴ ┴┴ ┴└─┘  └─┘┴ ┴┘└┘┴ ┴─┴┘└─┘  ╚╝╚═╝╚═╝╩ ╩═╩╝╚═╝╩╚═
+""")
+
+    maquina_barcos = fn.tablero
 elif fn.ataca_maquina() == False:
-    tablero =np.full((10, 10), " ")
-    print("Enhorabuena maquina has ganado")
-    jugador_barcos = tablero
+
+    print("""
+╔═╗╔╗╔╦ ╦╔═╗╦═╗╔═╗╔╗ ╦ ╦╔═╗╔╗╔╔═╗                  
+║╣ ║║║╠═╣║ ║╠╦╝╠═╣╠╩╗║ ║║╣ ║║║╠═╣                  
+╚═╝╝╚╝╩ ╩╚═╝╩╚═╩ ╩╚═╝╚═╝╚═╝╝╚╝╩ ╩                  
+┬ ┬┌─┐┌─┐  ┌─┐┌─┐┌┐┌┌─┐┌┬┐┌─┐  ╔╦╗╔═╗╔═╗ ╦ ╦╦╔╗╔╔═╗
+├─┤├─┤└─┐  │ ┬├─┤│││├─┤ │││ │  ║║║╠═╣║═╬╗║ ║║║║║╠═╣
+┴ ┴┴ ┴└─┘  └─┘┴ ┴┘└┘┴ ┴─┴┘└─┘  ╩ ╩╩ ╩╚═╝╚╚═╝╩╝╚╝╩ ╩                                        
+""")
+    jugador_barcos = fn.tablero
